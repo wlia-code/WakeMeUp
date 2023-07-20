7e0139a7a3f56c886a01bc5a16cb853bba0a0910
Wasim Alrawas <wassimrawwas@gmail.com>
2023-07-20 18:33:26 +0000

    add finish game function edit start game function add flip all cards at starting for short time edit the variables and deleted the timer function

--- assets/app.js
+++ assets/app.js
@@ -1,50 +1,37 @@
-//click event listener for the start game button
-document.querySelector(".start-game-button span").onclick = function () {
-    let yourName = prompt("Whats Your Name?");
-    if (yourName == null || yourName == "") {
-        document.querySelector(".player-name span").innerHTML = 'Unknown';
-    } else {
-        document.querySelector(".player-name span").innerHTML = yourName;
-    }
-    document.querySelector(".start-game-button").remove();
-};
-let durationTime = 1000;
-let gameBlocks = document.querySelector(".game-boards")
-let blocks = Array.from(gameBlocks.children)
-let orderRange = Array.from(Array(blocks.length).keys());
-shuffle(orderRange)
+// Variables
+const durationTime = 1000;
+const gameBlocks = document.querySelector(".game-boards");
+const blocks = Array.from(gameBlocks.children);
+const orderRange = Array.from(Array(blocks.length).keys());
+shuffle(orderRange);
+
+// Set order for the blocks and add click event listeners
 blocks.forEach((block, index) => {
     block.style.order = orderRange[index];
-
     block.addEventListener("click", function () {
-        flipCard(block)
-    })
+        flipCard(block);
+    });
 });
-// Flip Block Function
+
+//flip a card when clicked
 function flipCard(selectedBlock) {
     selectedBlock.classList.add('is-flipped');
-    let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains("is-flipped"))
+    let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains("is-flipped"));
     if (allFlippedBlocks.length === 2) {
         stopClicking();
         checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
     }
-};
-// Stop Clicking  Function
-function stopClicking() {
-
-    // Add Class No Clicking on Main Container
-    gameBlocks.classList.add('no-clicking');
-
-    // Wait Duration
-    setTimeout(() => {
-
-        // Remove Class No Clicking After The Duration
-        gameBlocks.classList.remove('no-clicking');
-
-    }, durationTime);
-
 }
-// Reset Boards Function
+
+//disable clicking on all blocks for a short duration
+function stopClicking() {
+    gameBlocks.classList.add('no-clicking');
+    setTimeout(() => {
+        gameBlocks.classList.remove('no-clicking');
+    }, durationTime);
+}
+
+//check if the two flipped blocks match
 function checkMatchedBlocks(firstBlock, secondBlock) {
     let triesElement = document.querySelector('.player-tries span');
     if (firstBlock.dataset.animals === secondBlock.dataset.animals) {
@@ -52,6 +39,7 @@ function checkMatchedBlocks(firstBlock, secondBlock) {
         secondBlock.classList.remove('is-flipped');
         firstBlock.classList.add('has-match');
         secondBlock.classList.add('has-match');
+        checkAllMatched();
     } else {
         triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
         setTimeout(() => {
@@ -61,7 +49,7 @@ function checkMatchedBlocks(firstBlock, secondBlock) {
     }
 }
 
-
+//shuffle an array
 function shuffle(array) {
     let current = array.length,
         temp,
@@ -75,3 +63,81 @@ function shuffle(array) {
     }
     return array;
 }
+
+//flip or unflip all cards based on the provided action
+function flipAllCards(action) {
+    blocks.forEach(block => {
+        if (action === "flip") {
+            block.classList.add('is-flipped');
+        } else if (action === "unflip") {
+            block.classList.remove('is-flipped');
+        }
+    });
+}
+
+// Get references to the necessary elements in the finish dialog
+const finishDialog = document.getElementById("finish-dialog");
+const finishMessage = document.getElementById("finish-message");
+const playAgainButton = document.getElementById("play-again-button");
+const finishButton = document.getElementById("finish-button");
+
+//handle the Play Again button
+function handlePlayAgainButtonClick() {
+    resetGame();
+    finishDialog.close();
+}
+
+//handle the "Finish" button 
+function handleFinishButtonClick() {
+    location.reload();
+}
+
+//check if all blocks have been matched
+function checkAllMatched() {
+    let allMatched = blocks.every(block => block.classList.contains('has-match'));
+    if (allMatched) {
+        finishMessage.textContent = "Thank you for playing!";
+        finishDialog.showModal();
+    }
+}
+
+//reset the game
+function resetGame() {
+    blocks.forEach(block => {
+        block.classList.remove('is-flipped', 'has-match');
+    });
+}
+
+playAgainButton.addEventListener("click", handlePlayAgainButtonClick);
+finishButton.addEventListener("click", handleFinishButtonClick);
+
+// start the game by flipping all cards initially and then unflipping them
+function startGame(playerName) {
+    document.querySelector(".player-name span").textContent = playerName;
+    document.querySelector(".player-tries span").textContent = "0";
+
+    document.querySelector(".start-game-overlay").remove();
+    flipAllCards("flip");
+
+    setTimeout(() => {
+        flipAllCards("unflip"); // Unflip all cards after 2.5 seconds
+    }, 2500);
+}
+
+// Event listener for the Start Game button and "Play as Guest" and blank input message
+document.getElementById("start-button").addEventListener("click", function () {
+    let yourName = document.getElementById("name-input").value.trim();
+    const errorMessageElement = document.querySelector(".error-message");
+
+    if (yourName === "") {
+        errorMessageElement.style.display = "block"; // Display error message if the player name is empty
+        return;
+    }
+
+    errorMessageElement.style.display = "none";
+    startGame(yourName);
+});
+
+document.getElementById("guest-button").addEventListener("click", function () {
+    startGame("Guest");
+});
